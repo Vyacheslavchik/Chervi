bde1ffcb6758e8f5d383d529ca0d39eb8774d917
Vyacheslavchik <clavik_2000@mail.ru>
2022-01-06 14:49:36 +0300

    Переделана сортровка карт. Теперь каждый игрок хранит у себя свою колоду. Сортировка раздает основную колоду по игрокам. Массивы заменены ArrayList

--- src/main/java/game/objects/GameTable.java
+++ src/main/java/game/objects/GameTable.java
@@ -1,44 +1,44 @@
 package game.objects;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 public class GameTable {
-    public static Card coloda[] = GameTable.makeColod();
+    private ArrayList<Card> coloda = GameTable.makeColod();
     public GameTable (String namePlayer){
         Player players [] = {new Player(namePlayer),new Player("Din"),new Player("Sam"),new Player("Kas")};
         getCardsToPlayers(players);
+
     }
 
-    private static Card[] makeColod(){
-        coloda = new Card[52];
+    private static ArrayList<Card> makeColod(){
+        ArrayList<Card>coloda = new ArrayList<>();
         for(int i=0,valueCards=0;i<4;i++){
             for(int q=0;q<13;q++,valueCards++){
-                if(i==0) coloda[valueCards]=new Card("H",q,1);
-                 else if(i==1) coloda[valueCards]=new Card("B",q,0);
-                 else if (i==2)coloda[valueCards]=new Card("K",q,0);
+                if(i==0) coloda.add(new Card("H",q,1));
+                 else if(i==1) coloda.add(new Card("B",q,0));
+                 else if (i==2)coloda.add(new Card("K",q,0));
                  else if(i==3){
-                     if(q==10)coloda[valueCards]=new Card("P",q,13);
-                     else coloda[valueCards]=new Card("P",q,0);
+                     if(q==10)coloda.add(new Card("P",q,13));
+                     else coloda.add(new Card("P",q,0));
                 }
             }
         }
         return coloda;
     }
     private void getCardsToPlayers(Player players []){
-          for(int q=0,w=1,e=2,r=3;r<52;q+=4,w+=4,e+=4,r+=4){
-            coloda[q].host=players[0];
-            coloda[w].host=players[1];
-            coloda[e].host=players[2];
-            coloda[r].host=players[3];
-        }
           Random rnd = new Random();
-          for(int i=0;i<52;i++){
-              int a=rnd.nextInt(51);
-              int b=rnd.nextInt(51);
-              Player p=coloda[a].host;
-              coloda[a].host=coloda[b].host;
-              coloda[b].host=coloda[a].host;
+          int q=51;
+          int a;
+          for (int w=0;w<4;w++)
+          for (int i=0;i<13;i++,q--){
+              if(q!=0) a=rnd.nextInt(q);
+              else a=0;
+              coloda.get(a).host=players[w];
+              players[w].cards.add(coloda.get(a));
+              coloda.remove(a);
           }
+
     }
 
     private Card vziatka (Card card1,Card card2,Card card3,Card card4){
--- src/main/java/game/objects/Player.java
+++ src/main/java/game/objects/Player.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 public class Player {
     private String name;
     int points;
+    ArrayList<Card> cards=new ArrayList<>();
     public Player(String name){
         this.name=name;
         this.points=0;
